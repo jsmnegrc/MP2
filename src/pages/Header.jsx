2139dac0dcd4644cc80e8e60526508f4edd8cc09
araserdan <ara.serdan18@gmail.com>
2023-09-17 04:56:56 +0800

    fixed logo size, social media icons, added a video for the hero section but still for fixing since showing of content was not successful

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,48 +1,62 @@
-import Button from "react-bootstrap/Button";
+import React from 'react';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import Logo from "../assets/UB Logo.png"
+import './Header.css';
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand as={Link} to="./">
-        <img src={Logo} alt="Logo" />
+      <Container id="nav">
+        <div id='UBlogo'>
+        <Navbar.Brand as={Link} to="./" className='navi'>
+        <img src={Logo} alt="Logo" className="Logo" />
         </Navbar.Brand>
+        </div>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
+          <div id="navLinks">
           <Nav className="m-auto ">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link as={Link} to="/" className='hme'>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/about">
+            <Nav.Link as={Link} to="/about" className='abt'>
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/community">
+            <Nav.Link as={Link} to="/community" className='com'>
               Community
             </Nav.Link>
-            <Nav.Link as={Link} to="/pricing">
+            <Nav.Link as={Link} to="/pricing" className='price'>
               Pricing
             </Nav.Link>
-            <Nav.Link as={Link} to="/news">
+            <Nav.Link as={Link} to="/news" className='news'>
               News
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact">
+            <Nav.Link as={Link} to="/contact" className='con'>
               Contact
             </Nav.Link>
           </Nav>
-          
+          </div>
+        <div id="social">
         <a href="/">
-          <FaFacebookSquare size={24} />
+          <FaFacebookSquare size={24} className='fb' />
         </a>
         <a href="/">
-          <FaInstagramSquare size={24} />
+          <FaInstagramSquare size={24} className='ig' />
         </a>
+        <a href="/">
+          <FaYoutube size={24} className='yt' />
+        </a>
+        <a href="/">
+          <FaTwitterSquare size={24} className='tw' />
+        </a>
+        </div>  
         </Navbar.Collapse>
       </Container>
     </Navbar>
