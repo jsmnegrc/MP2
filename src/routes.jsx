c52c69cb8596263f38226c15ab960b39d05244be
araserdan <ara.serdan18@gmail.com>
2023-09-16 22:59:06 +0800

    added Community folder and file, .env file

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,25 +1,36 @@
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Pricing from "./pages/Pricing/Pricing";
-import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Community from "./pages/Community/Community"
+import News from "./pages/News/News"
+import Contact from "./pages/Contact/Contact"
+import Pricing from "./pages/Pricing/Pricing"
 
 const routes = [
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/pricing",
-    element: <Pricing />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-];
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+    {
+      path: "/community",
+      element: <Community />,
+    },
+    {
+        path: "/news",
+        element: <News />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />,
+    },
+    {
+        path: "/pricing",
+        element: <Pricing />,
+    },
+                                           
+]
 
 export default routes;
