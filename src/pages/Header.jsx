e469b069863e59b09dceb70d1866edb3e339e9e2
araserdan <ara.serdan18@gmail.com>
2023-09-16 23:47:11 +0800

    added company logo to header/navbar

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,13 +3,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import Logo from "../assets/UB Logo.png"
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          URBAN <br /> HOMES
+        <Navbar.Brand as={Link} to="./">
+        <img src={Logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -20,8 +21,14 @@ const Header = () => {
             <Nav.Link as={Link} to="/about">
               About
             </Nav.Link>
+            <Nav.Link as={Link} to="/community">
+              Community
+            </Nav.Link>
             <Nav.Link as={Link} to="/pricing">
               Pricing
+            </Nav.Link>
+            <Nav.Link as={Link} to="/news">
+              News
             </Nav.Link>
             <Nav.Link as={Link} to="/contact">
               Contact
