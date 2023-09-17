6c12734e0bc8d623573529a297f97b26af048ae3
araserdan <ara.serdan18@gmail.com>
2023-09-17 21:23:04 +0800

    linked google fonts in index.html, styled Hero section, modified some font colors

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
 import VideoPoster from "../../../assets/video-poster.jpg";
 import Video from "../../../assets/Aerial Shot.mp4";
 import "./Hero.css";
@@ -9,37 +9,52 @@ const Hero = () => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.load();
-      videoRef.current.play();
+    const video = videoRef.current;
+
+    if (video) {
+
+      const playPromise = video.play();
+
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => {
+
+          })
+          .catch(error => {
+
+          });
+      }
     }
   }, []);
 
   return (
     <section
-    id="Hero"
-    className="d-flex justify-content-center align-items-center"
-  >
-    <Container>
-      <Row id="Hero-Vid">
-        <div className="video-container">
-          <video
-            ref={videoRef}
-            width="100%"
-            poster={VideoPoster}
-            className="v-poster"
-          >
-            <source src={Video} type="video/mp4" alt="UB aerial" />
-          </video>
-        </div>
-      </Row>
-          <Row className="align-self-center">
-            <h3>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
-              dolore laudantium ipsa reprehenderit modi nisi, vitae distinctio
-              repellendus voluptas autem.
-            </h3>
-          </Row>
+      id="Hero"
+      className="d-flex justify-content-center align-items-center"
+    >
+      <Container>
+        <Row id="Hero-Vid">
+          <div className="video-container">
+            <video
+              ref={videoRef}
+              width="100%"
+              poster={VideoPoster}
+              className="v-poster"
+              autoPlay
+              muted
+            >
+              <source src={Video} type="video/mp4" alt="UB aerial" />
+            </video>
+          </div>
+        </Row>
+        <Row className="align-self-center" id="Hero-Text">
+          <h1 className="hero-heading">
+            Urban Haven
+          </h1>
+          <h3 className="hero-subheading">
+          “Where Dreams Find a Home”
+          </h3>
+        </Row>
       </Container>
     </section>
   );
