d1a325260a8f8087260746033ca299c6323f86b6
araserdan <142366410+araserdan@users.noreply.github.com>
2023-09-17 16:02:02 +0800

    Update Header.jsx
    changed logo

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import Logo from "../assets/UB Logo.png";
+import Logo from "../assets/logo_company.png";
 
 const Header = () => {
   return (
