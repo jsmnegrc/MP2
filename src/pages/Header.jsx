57f2032f479b0f51e6d257fe374e034117c90778
araserdan <ara.serdan18@gmail.com>
2023-09-17 15:52:49 +0800

    fixed navbar links to center, added hover, shadow effect and color. Added spacing between navlinks and social links. changed logo with company name.

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import Logo from "../assets/UB Logo.png"
+import Logo from "../assets/logo_company.png"
 import './Header.css';
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
@@ -22,8 +22,8 @@ const Header = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
 
-          <div id="navLinks">
-          <Nav className="m-auto ">
+          <div className="d-flex mx-auto" id="navLinks">
+          <Nav className="ml-auto ">
             <Nav.Link as={Link} to="/" className='hme'>
               Home
             </Nav.Link>
