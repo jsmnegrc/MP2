145d1d75f93a4b9e074f0979ee4e01e524f0be2f
araserdan <ara.serdan18@gmail.com>
2023-09-17 19:54:26 +0800

    changed code in hero sectionfor the video to be the background, used usestate

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -1,23 +1,38 @@
-
+import React, { useRef, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from 'react-bootstrap/Row';
-import Col from "react-bootstrap/Col";
 import VideoPoster from "../../../assets/video-poster.jpg";
 import Video from "../../../assets/Aerial Shot.mp4";
 import "./Hero.css";
 
 const Hero = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.load();
+      videoRef.current.play();
+    }
+  }, []);
+
   return (
     <section
-      id="Hero"
-      className="d-flex justify-content-center align-items-center"
-    >
-      <Container>
-        <Row id="Hero-Vid">
-            <video width="100%" controls poster={VideoPoster} className="v-poster">
-                <source src={Video} type="video/mp4" alt="UB aerial" className="Aerial" />
-            </video>
-        </Row>
+    id="Hero"
+    className="d-flex justify-content-center align-items-center"
+  >
+    <Container>
+      <Row id="Hero-Vid">
+        <div className="video-container">
+          <video
+            ref={videoRef}
+            width="100%"
+            poster={VideoPoster}
+            className="v-poster"
+          >
+            <source src={Video} type="video/mp4" alt="UB aerial" />
+          </video>
+        </div>
+      </Row>
           <Row className="align-self-center">
             <h3>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
