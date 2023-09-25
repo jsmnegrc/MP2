71051150d684380f731e3d4e57ad1b2802e64340
araserdan <ara.serdan18@gmail.com>
2023-09-25 15:07:50 +0800

    fixed merge errors

--- src/library/http.js
+++ src/library/http.js
@@ -6,14 +6,8 @@ const options = {
     "X-RapidAPI-Key": import.meta.env.VITE_KEY,
     "X-RapidAPI-Host": import.meta.env.VITE_HOST,
   },
-<<<<<<< HEAD
-});
-
-export default axios
-=======
 };
 
 const http = axios.create(options);
 
 export default http;
->>>>>>> 235653c44d6fbb59325789172fc68c091d9293d2
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,6 @@
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-<<<<<<< HEAD
-import Pricing from "./pages/Pricing/Pricing";
-=======
 import Property from "./pages/Properties/Property";
-import Community from "./pages/Community/Community";
->>>>>>> 235653c44d6fbb59325789172fc68c091d9293d2
 import News from "./pages/News/News";
 import Contact from "./pages/Contact/Contact";
 import Community from "./pages/Community/Community";
