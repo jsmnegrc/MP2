6982a6b0dab918d4b68462d0b8043d9f702ba1f1
araserdan <ara.serdan18@gmail.com>
2023-09-29 01:17:10 +0800

    contact.css modification

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -59,7 +59,7 @@ const ContactForm = () => {
   return (
     <Container className="p-5">
       <h1 className="fw-bold">Contact Us</h1>
-      <p>
+      <p className="ctext">
         Thank you for your interest in Urban Haven Real Estate. We are dedicated
         to assisting you with all your real estate requirements, whether you're
         interested in buying, selling, or renting a property. Please feel free
